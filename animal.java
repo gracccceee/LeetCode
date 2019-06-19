a43fb4436a2ef74bbb8776096e703010c00cf3dc
gracccceee <51980784+gracccceee@users.noreply.github.com>
2019-06-19 13:36:17 +0930

    Add files via upload

---
+++ animal.java
@@ -0,0 +1,7 @@
+package day11_helpbook;
+
+public class animal {
+	public static void main(String[]args) {
+		animal a = new animal();
+	}
+}
---
+++ jump.java
@@ -0,0 +1,13 @@
+package day11_helpbook;
+/**
+ * 这是跳高接口
+ * @author gracc
+ *@version v1.0
+ *
+ */
+public interface jump {
+	/**
+	 * 这是跳高功能
+	 */
+	public abstract void jump();
+}
